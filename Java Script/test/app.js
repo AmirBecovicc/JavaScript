e71c5ad05ce2b6558b2fed0d16b6eec6b26ab748
AmirBecovic <160780572+AmirBecovicc@users.noreply.github.com>
2024-03-25 21:16:08 +0100

    test

---
+++ Java Script/test/app.js
@@ -0,0 +1,9 @@
+const array1 = [
+    { id: "333222111", name: "Sulejman", age: "22" },
+    { id: "321123432", name: "Stefan", age: "25" },
+    { id: "222312543", name: "Semir", age: "32" },
+  ];
+  function getObjects(array, value) {
+    return array.filter((obj) => obj.name === value)
+  }
+  console.log(getObjects(array1, "Semir"));
